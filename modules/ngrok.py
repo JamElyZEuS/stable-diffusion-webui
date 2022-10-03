2148c005c7adc4a7b72adf43b71e95579fb32658
JamnedZ <zaguzinegor@gmail.com>
2022-10-04 03:20:17 +0700

    fixed merged lines

--- modules/ngrok.py
+++ modules/ngrok.py
@@ -9,5 +9,5 @@ def connect(token, port):
         print(f'Invalid ngrok authtoken, ngrok connection aborted.\n'
               f'Your token: {token}, get the right one on https://dashboard.ngrok.com/get-started/your-authtoken')
     else:
-        print(f'ngrok connected to localhost:{port}! URL: {public_url}'
+        print(f'ngrok connected to localhost:{port}! URL: {public_url}\n'
                'You can use this link after the launch is complete.')
