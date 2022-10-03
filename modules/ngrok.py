745d7546f8a95c5720a49ab8600622a7ea53d920
JamnedZ <zaguzinegor@gmail.com>
2022-10-04 03:00:55 +0700

    fixed try-except in ngrok.py

--- modules/ngrok.py
+++ modules/ngrok.py
@@ -5,7 +5,7 @@ def connect(token, port):
     conf.get_default().auth_token = token
     try:
         public_url = ngrok.connect(port).public_url
-    except exception:
+    except exception.PyngrokNgrokError:
         print(f'Invalid ngrok authtoken, ngrok connection aborted.\n'
               f'Your token: {token}, get the right one on https://dashboard.ngrok.com/get-started/your-authtoken')
     else:
